90292497044a019b85dac7c64860c27757adb5c9
John Strange <john.strange.nz@gmail.com>
2019-08-14 16:32:15 +1200

    Shooting Basic Bullet Hell

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -39,23 +39,27 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].position, speed * Time.deltaTime);
-
-        if(Vector2.Distance(transform.position, moveSpots[i].position) < 0.2f)
+        if(movementType == MovementType.Patrol)
         {
-            if (waitTime <= 0)
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].position, speed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, moveSpots[i].position) < 0.2f)
             {
-                i++;
-                waitTime = startWaitTime;
-                if(i == moveSpots.Length)
+                if (waitTime <= 0)
                 {
-                    i = 0;
+                    i++;
+                    waitTime = startWaitTime;
+                    if (i == moveSpots.Length)
+                    {
+                        i = 0;
+                    }
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
                 }
             }
-            else
-            {
-                waitTime -= Time.deltaTime;
-            }
+
         }
     }
 
