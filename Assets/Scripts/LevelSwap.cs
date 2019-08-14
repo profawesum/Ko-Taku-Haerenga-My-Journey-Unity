bbeaa9bc6d01fc438c93e43f2c15411a3eb660b0
profawesum <profawesum@gmail.com>
2019-08-14 16:46:02 +1200

    Made menu and level transitions.

---
+++ Assets/Scripts/LevelSwap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSwap : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "finish") {
+            Application.LoadLevel("sceneToLoad");
+        }
+    }
+}
---
+++ Assets/Scripts/MenuMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMove : MonoBehaviour
+{
+
+
+    public void play() { 
+        //load the first level
+        Application.LoadLevel(1);
+    }
+
+    public void quit() {
+        //quit the game
+        Application.Quit();
+    }
+
+
+
+}
