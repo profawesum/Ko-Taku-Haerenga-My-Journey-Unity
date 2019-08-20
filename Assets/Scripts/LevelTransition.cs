d8dcfa37ebfca43fc2918ee0b186a2e868747183
Profawesum <profawesum@gmail.com>
2019-08-20 16:28:18 +1200

    Updates to levels
    Updates to levels and made it so the level transition will work and there is a basic menu which will start the game and also let the player quit the game

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string levelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish") {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuMove : MonoBehaviour
+{
+    public void play() {
+
+        Application.LoadLevel(1);
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+
+
+
+}
+
