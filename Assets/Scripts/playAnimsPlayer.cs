7a8e297f5966f00f1a64d7616615961b80a1cfc6
prof <profawesum@gmail.com>
2019-08-22 11:27:59 +1200

    Added in sfx and music and other fixes

---
+++ Assets/Scripts/playAnimsPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimsPlayer : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            anim.SetBool("isRun", true);
+        }
+        else {
+            anim.SetBool("isRun", false);
+        }
+    }
+}
