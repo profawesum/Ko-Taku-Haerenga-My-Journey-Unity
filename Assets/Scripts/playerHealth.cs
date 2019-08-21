3d2328b5ffc29b7fa99cbb54ffe3c8101dae0545
profawesum <profawesum@gmail.com>
2019-08-21 17:28:24 +1200

    Made new minor totem effects
    minor totems will clear the screen for a small time

--- Assets/Scripts/playerHealth.cs
+++ Assets/Scripts/playerHealth.cs
@@ -14,18 +14,29 @@ public class playerHealth : MonoBehaviour
 
     public GameObject player;
 
+    public GameObject destructor;
+
     public Image healthBar;
     public Image healthBar2;
+
+    int timer;
 
     private void Awake()
     {
         currentHealth = 100;
+        destructor.SetActive(false);
     }
 
     // Update is called once per frame
     void Update(){
         if (currentHealth <= 0) {
             Application.LoadLevel(Application.loadedLevel);
+        }
+
+        timer -= 1;
+
+        if (timer <= 0) {
+            destructor.SetActive(false);
         }
 
 
@@ -49,6 +60,11 @@ public class playerHealth : MonoBehaviour
         {
             pickupTotem.sisterTotem.enabled = true;
             Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "minorTotem") {
+            destructor.SetActive(true);
+            Destroy(collision.gameObject);
+            timer += 20;
         }
     }
 
